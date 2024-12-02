cf74c74855c6a259b8cf05a5e917b30029405211
Konstantinos Kampourelis <konkamp94@gmail.com>
2024-12-03 01:34:38 +0200

    parse csv and populate db script

---
+++ seed-db.js
@@ -0,0 +1,45 @@
+const fs = require('fs');
+const csvParser = require('csv-parser');
+const { Client } = require('pg');
+
+const client = new Client({
+  host: 'localhost',
+  port: 5432,
+  database: 'openlayers',
+  user: 'postgres',
+  password: 'postgres',
+});
+client.connect();
+
+const insertPredefinedLocations = async (location) => {
+  const query = `
+    INSERT INTO predefined_location(name, coordinates)
+    VALUES ($1, ST_SetSRID(ST_MakePoint($2, $3), 4326));
+  `;
+  console.log(location);
+  await client.query(query, [location.name, location.lng, location.lat]);
+};
+
+const parseCsvAndSaveLocation = () => {
+  const result = [];
+
+  fs.createReadStream('./locations.csv')
+    .pipe(csvParser())
+    .on('data', (data) => {
+      result.push({
+        name: data.name,
+        lng: parseFloat(data.x),
+        lat: parseFloat(data.y),
+      });
+    })
+    .on('end', async () => {
+      result.forEach(async (location) => {
+        await insertPredefinedLocations(location);
+      });
+    })
+    .on('close', () => {
+      console.log('CSV file successfully processed');
+    });
+};
+
+parseCsvAndSaveLocation();
